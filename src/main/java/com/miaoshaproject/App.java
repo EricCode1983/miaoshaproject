f0154e1b3fccc6e6af5e2b87b0478f967bbfc1fd
Eric Chen <eric@bblab.biz>
2018-12-25 22:17:36 +0700

    select user

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -1,7 +1,11 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
 @RestController
+@MapperScan("com.miaoshaproject.dao")
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
         return "Hello World";
+    }
+    @RequestMapping("/test")
+    public String test(){
+        UserDO userDO=userDOMapper.selectByPrimaryKey(1);
+        if(userDO==null)
+        {
+            return "user not exists";
+        }
+        else {
+            return userDO.getName();
+        }
+
     }
 
     public static void main( String[] args )
